e2c81cd051112d1f7cf524b5ec07a1f2641e4445
monojit.laha@gmail.com <monojit.laha@gmail.com>
2022-05-06 23:00:07 +0530

    Added Test Case

--- SkillTrackerService.Tests/ControllerTests/AdminControllerTest.cs
+++ SkillTrackerService.Tests/ControllerTests/AdminControllerTest.cs
@@ -1,8 +1,9 @@
+using System.Collections.Generic;
 using Microsoft.Extensions.Logging;
 using Moq;
 using SkillTrackerService.Controllers;
+using SkillTrackerService.Models;
 using SkillTrackerService.Services;
-using System;
 using Xunit;
 
 namespace SkillTrackerService.Tests.ControllerTests
@@ -21,9 +22,12 @@ namespace SkillTrackerService.Tests.ControllerTests
         }
    
         [Fact]
-        public void Test1()
+        public async void Get_Result_PositiveTest()
         {
-
+            _mockProfileService.Setup(x => x.GetAsync()).ReturnsAsync(new List<Profile> { new Profile { Name = "Test", AssociateId = "1234", Email = "Test@gmail.com", Mobile = "9876543212" } });
+            var output = await _adminController.Get();
+            Assert.NotNull(output.Value);
+            Assert.Equal("Test", output.Value[0].Name);            
         }
     }
 }
