e3f4252000a110bbc5593dd8c5960ea2d4bcad79
swadhin <swadhin.mukherjee@cognizant.com>
2022-05-14 13:07:08 +0530

    ELK Changes

--- SkillTrackerService/Program.cs
+++ SkillTrackerService/Program.cs
@@ -7,20 +7,80 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using System.Reflection;
+
 namespace StockMarketService
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+			//configure logging first
+			ConfigureLogging();
 
+			CreateHostBuilder(args).Build().Run();
+		}
+
+
+		/*
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
-    }
+		*/
+
+
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStartup<Startup>();
+				})
+				.ConfigureAppConfiguration(configuration =>
+				{
+					configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+					configuration.AddJsonFile(
+						$"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+						optional: true);
+				})
+				.UseSerilog();
+
+		private static void ConfigureLogging()
+		{
+			var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+			var configuration = new ConfigurationBuilder()
+				.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+				.AddJsonFile(
+					$"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+					optional: true)
+				.Build();
+
+			Log.Logger = new LoggerConfiguration()
+				.Enrich.FromLogContext()
+				.Enrich.WithMachineName()
+				.WriteTo.Debug()
+				.WriteTo.Console()
+				.WriteTo.Elasticsearch(ConfigureElasticSink(configuration, environment))
+				.Enrich.WithProperty("Environment", environment)
+				.ReadFrom.Configuration(configuration)
+				.CreateLogger();
+		}
+
+		private static ElasticsearchSinkOptions ConfigureElasticSink(IConfigurationRoot configuration, string environment)
+		{
+			return new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
+			{
+				AutoRegisterTemplate = true,
+				IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+			};
+		}
+	}
+
+
+
 }
